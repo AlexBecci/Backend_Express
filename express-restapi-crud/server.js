909421bf0762e9e761b970a5822827e38d609540
Alex Becci <alex.becci@bingopilar.corp>
2024-09-13 11:30:20 -0300

    POST, GET

--- express-restapi-crud/server.js
+++ express-restapi-crud/server.js
@@ -2,16 +2,23 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
-
+//forzando datos como si buena traido de la base
+const products = [
+    { id: 1, name: 'Alex', price: 3000 }
+]
 app.use(morgan('dev'))
-
+app.use(express.json())
 //get
 app.get('/products', (req, res) => {
-    res.send('Obteniendo productos')
+    /* res.send('Obteniendo productos') */
+    res.json(products)
 })
-//post
+//post 
 app.post('/products', (req, res) => {
-    res.send('Creando productos')
+    const newProduct = { ...req.body, id: products.length + 1 }
+    products.push(newProduct)
+    /*  res.send(`Producto ${newProduct} creado con exito`) */
+    res.send(newProduct)
 })
 //put
 app.put('/products', (req, res) => {
@@ -23,7 +30,20 @@ app.delete('/products', (req, res) => {
 })
 //get
 app.get('/products/:id', (req, res) => {
-    res.send('Obteniendo un producto')
+    console.log(req.params.id)
+    const { id } = req.params
+    console.log(id)
+    const productFound = products.find((product) => {
+        return product.id === parseInt(id)
+    })
+    if (!productFound) {
+        return res.status(404).json({
+            message: "Product Not Found"
+        })
+    }
+    console.log(productFound)
+    res.send(productFound)
+    /* res.send(`Obteniendo un producto  ${id}`) */
 })
 
 app.listen(3000)
